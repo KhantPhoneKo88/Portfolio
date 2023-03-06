60497b87940cbd9daef0bf319b9b731aab3a7870
KhantPhoneKo88 <khantphoneko88@gmail.com>
2023-03-07 00:53:34 +0700

    Tailwind Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        open: "Open Sans",
+        poppin: "Poppins",
+        bree: "Bree Serif",
+      },
+    },
+  },
+  plugins: [],
+};
